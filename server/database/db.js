b0f17b17a3d4453ebc25ae8769cb06b459513c24
Abhitabh <abhitabhkp.ic.20@nitj.ac.in>
2023-06-04 15:32:28 +0530

    first commit

---
+++ server/database/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD; 
+
+const Connection = () => {
+    const DB_URI = "mongodb://localhost:27017/gmail";
+    try {
+        mongoose.connect(DB_URI, {useNewUrlParser: true,
+            useUnifiedTopology: true,});
+        mongoose.set('strictQuery', false);
+        console.log('Database connected sucessfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message)
+    }
+}
+
+export default Connection;
